340adc80e5f9fb66b9682da103eeab13785a09e3
MeshackYaro <yaromeshack@gmail.com>
2024-03-08 09:00:11 +0100

    update

--- test/bankTest/BankTest.java
+++ test/bankTest/BankTest.java
@@ -2,6 +2,7 @@ package bankTest;
 
 import bank.Account;
 import bank.Bank;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,12 +11,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankTest {
     ArrayList<Account> accounts = new ArrayList<>();
+    private Bank myBank;
+    @BeforeEach
+    public void myBank() {
+        myBank = new Bank("Mavericks");
+    }
 
     @Test
-    public void bankCanRegisterACustomer() {
-        Bank myBank = new Bank("Mavericks");
+    public void customerCanBeRegisteredInTheBank_registerCustomerTest() {
         Account myAccount = myBank.registerCustomer("firstName", "lastName", "1234");
         assertEquals(1, myBank.getNumberOfCustomers());
+    }
+    @Test
+    public void moreThanOneCustomerCanBeRegisteredInTheBank_registerCustomerTest() {
+        Account myAccount = myBank.registerCustomer("firstName", "lastName" , "pin");
+        Account myAccount1 = myBank.registerCustomer("firstName1", "lastName1" , "pin");
+        assertEquals(2, myBank.getNumberOfCustomers());
+    }
+    @Test
+    public void accountCanBeFoundInTheBank_findAccountTest() {
+        Account myAccount = myBank.registerCustomer("firstName", "lastName", "pin");
+
 
     }
+
 }
